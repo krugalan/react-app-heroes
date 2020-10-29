7cbf38970487c9b5648511ad94d67ee5173e4ffc
KrugAlan <krugalan@gmail.com>
2020-10-29 18:12:59 -0300

    act

---
+++ docs/precache-manifest.51c7bbe3d7f9a1898498356cff891304.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54b816542d28f786a2b1c6a8dbf4942d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c4c69c31c91a7764d94",
+    "url": "/static/js/2.5f0d9112.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/static/js/2.5f0d9112.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec153943480afddf0395",
+    "url": "/static/js/main.0ee651c2.chunk.js"
+  },
+  {
+    "revision": "94d7ca25d4df19621824",
+    "url": "/static/js/runtime-main.b7f813af.js"
+  }
+]);
